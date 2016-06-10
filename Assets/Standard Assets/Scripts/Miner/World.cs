b8e3bb9785f4891f5f3181f88caa85ae8db421a6
Zekye <Zekye@live.fi>
2016-06-11 01:30:16 +0300

    Miner
    Added World Object

---
+++ Assets/Standard Assets/Scripts/Miner/World.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class World : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject plane = GameObject.CreatePrimitive (PrimitiveType.Plane);
+		Instantiate (plane);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
