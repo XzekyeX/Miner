53514d06bf13ec7915dfd59b231034058bbbd0b5
Zekye <Zekye@live.fi>
2016-06-11 02:08:02 +0300

    Visual Studio Sync
    Mono dev has been changed to Visual Studio

--- Assets/Standard Assets/Scripts/Miner/World.cs
+++ Assets/Standard Assets/Scripts/Miner/World.cs
@@ -6,7 +6,6 @@ public class World : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		GameObject plane = GameObject.CreatePrimitive (PrimitiveType.Plane);
-		Instantiate (plane);
 	}
 	
 	// Update is called once per frame
